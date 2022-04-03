32f63a19160ae062770ea3b404a64e09d44b1561
timrusin <timrusin@gmail.com>
2022-04-03 16:43:25 -0500

    intro to jsx and dynamic lists complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'comment one'},
+        {id: 2, text: 'comment two'},
+        {id: 3, text: 'comment three'},
+    ]
+
+    const loadung = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'> 
+      <h1>{ title.toUpperCase() }</h1>
+      <p>{ body }</p>
+
+    <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+    </div>
+      
     </div>
   );
 }
 
-export default App;
+export default App
