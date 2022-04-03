54f2e16b42cc72a6255a4ab13d7bf1c54690b471
timrusin <timrusin@gmail.com>
2022-04-03 17:15:13 -0500

    updated last commit with comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
-// import ReactDOM  from "react-dom";
 import * as ReactDOMClient from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOMClient.createRoot(document.querySelector('#root')).render(
+//this is the new way of rendering the root from Index.html with react18
+ReactDOMClient.createRoot
+(document.querySelector("#root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-
-
-// ReactDOM.render(
-//   document.getElementById("root")
-// );
+  </React.StrictMode>
+);
