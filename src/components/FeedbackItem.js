27cab5f52ef5340a22a5c5b57c4ff0ae1f3d7eb5
timrusin <timrusin@gmail.com>
2022-04-03 17:59:00 -0500

    about to create app level state video 15

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -4,17 +4,10 @@ const FeedbackItem = () => {
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is an example of feedback item')
 
-  const handleClick = () => {
-    setRating((prev)=> {//cool way to change state with 'prev'
-      console.log(prev)
-      return prev - 10
-    })
-  }
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
