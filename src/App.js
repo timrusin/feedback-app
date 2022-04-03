43d949a40d35f748f9cdf5f60bc396897d1b81a9
timrusin <timrusin@gmail.com>
2022-04-03 16:56:29 -0500

    bulding the header

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'comment one'},
-        {id: 2, text: 'comment two'},
-        {id: 3, text: 'comment three'},
-    ]
-
-    const loading = false
-    const showComments = true
-
-    const commentBlock = (
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
-    );
-
-   if (loading) return <h1>Loading...</h1>
-
-  return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
-    </div>
-  );
+  return <ha>My App</ha>;
 }
 
 export default App
