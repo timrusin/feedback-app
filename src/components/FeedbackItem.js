015f648791ffcc5c7e03e939689cc184e9af22b8
timrusin <timrusin@gmail.com>
2022-04-03 17:57:43 -0500

    playing around with 'prev' in sate

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -3,10 +3,18 @@ import { useState } from "react";
 const FeedbackItem = () => {
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is an example of feedback item')
+
+  const handleClick = () => {
+    setRating((prev)=> {//cool way to change state with 'prev'
+      console.log(prev)
+      return prev - 10
+    })
+  }
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
