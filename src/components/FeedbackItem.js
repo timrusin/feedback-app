a60fe6d5c5b09f2eee1223feefa6bdc19d2292b4
timrusin <timrusin@gmail.com>
2022-04-04 23:42:55 -0500

    pulled in react-icons and used font awesome FA times for our x for delete

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,10 +1,19 @@
 import Card from "./shared/Card";
 import PropTypes from 'prop-types'
+import {FaTimes} from 'react-icons/fa'
 
 const FeedbackItem = ( {item} ) => {
+
+  const handleClick = (id) => {
+    console.log(id)
+  }
+
   return (
     <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes onClick={()=>handleClick(item.id)}color='purple'/>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
