cf07f5e490434d8e8d3cb46ebda864a0e682b522
timrusin <timrusin@gmail.com>
2022-04-04 23:04:23 -0500

    did some conditinal in-line styling in the Card component instead

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Card = ( {children, reverse} ) => {
   return (
-    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    // <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    <div className='card'
+    style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+    }}>
+        {children}
+    </div>
   )
 }
 
