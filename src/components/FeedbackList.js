24041a1e64aa55bef9ffdcf1c0a38e9315499fee
timrusin <timrusin@gmail.com>
2022-04-06 13:13:01 -0500

    added animations using framer-motion for feedbackList

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const FeedbackList = ( {feedback, handleDelete} ) => {
     if(!feedback || feedback.length===0) {
         return <p>no feedback yet</p>
     }
-  return (
-    <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
-      ))}
-    </div>
-  );
+    return (
+      <div className="feedback-list">
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div 
+            key={item.id}
+            initial = {{opacity: 0}}
+            animate = {{opacity: 1}}
+            exit = {{opacity: 0}}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </div>
+    );
+  //version withought animation
+  // return (
+  //   <div className="feedback-list"> 
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 }
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(//we could hacve also just left this as Array, but this gets more specific
