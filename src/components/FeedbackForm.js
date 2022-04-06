5e8cca34f6ed9614d02d28d1be53b86daa659613
timrusin <timrusin@gmail.com>
2022-04-06 12:46:20 -0500

    created hadlesubmit for form

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -24,9 +24,19 @@ const FeedbackForm = () => {
         console.log(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if(text.trim().length > 10){
+        const newFeedback = {
+          text,
+          rating,
+        }
+        console.log(newFeedback)
+      }
+    }
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating)=>setRating(rating)}/>
         <div className="input-group">
